52b35e68581b42d733d7159df3e13f6853b7d27b
JJ <jonjolivette777@aol.com>
2019-06-12 12:28:44 -0700

    addition of comments and media/content assets

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+/* =================================================== *
+Project: Random Quote Generator (MAIN LOGIC - VANILLA JAVASCRIPT)
+Author: Jonathan J. Jolivette
+Created: 6.12.19
+Version: 1.1
+* =================================================== */
+
+// quotes element is an array of objects that stores all the key value pairs...
+// used for inputting data/content onto DOM
+// An additional "image" property added to the quote objects
+// Image property is source of background image
+
+
 const quotes = [
   {
     quote:
@@ -5,24 +18,28 @@ const quotes = [
     citation: "Malcolm X",
     source: "Speech, Prospects for Freedom",
     year: 1965,
-    image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/malcolm-x-thinks-jovemini-art.jpg"
+    image: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/malcolm-x-thinks-jovemini-art.jpg",
+    id: 1
   },
+
   {
     quote:
       "I am an invisible man...I am a man of substance, of flesh and bone, fiber and liquids—and I might even be said to possess a mind. I am invisible, understand, simply because people refuse to see me.",
     citation: "Ralph Ellison",
     source: "Invisible Man",
     year: 1952,
-    image: "https://image.pbs.org/poster_images/assets/Intro_Ellison_still3.png"
+    image: "https://image.pbs.org/poster_images/assets/Intro_Ellison_still3.png",
+    id: 2
   },
 
   {
     quote:
-      "Herein lies the tragedy of the age: not that men are poor—all men know something of poverty; not that men are wicked—who is good? Not that men are ignorant—what is truth? Nay, but that men know so little of men.",
+      "There is in this world no such force as the force of a person determined to rise. The human soul cannot be permanently chained.",
     citation: "W.E.B. Du Bois",
     source: "The Souls of Black Folk",
     year: 1903,
-    image: "http://yourblackworld.net/wp-content/uploads/2017/02/Screenshot-331-1000x600.png"
+    image: "/Users/johnhenry/Desktop/techDegree/unit1/techdegree-project-1/media/web-dubois.jpeg",
+    id: 3
   },
 
   {
@@ -31,7 +48,8 @@ const quotes = [
     citation: "Maya Angelou",
     source: "The Heart of a Woman",
     year: 1981,
-    image: "https://static1.squarespace.com/static/51734e04e4b08db710716119/5a4b7e6571c10bd7b36d5c7d/5a4b7e6571c10bd7b36d5c90/1510978279747/maya-angelou-she-was-spe-012.jpg"
+    image: "https://static1.squarespace.com/static/51734e04e4b08db710716119/5a4b7e6571c10bd7b36d5c7d/5a4b7e6571c10bd7b36d5c90/1510978279747/maya-angelou-she-was-spe-012.jpg",
+    id: 4
   },
 
   {
@@ -40,7 +58,8 @@ const quotes = [
     citation: "Langston Hughes",
     source: "The Negro Artist and the Racial Mountain, The Nation",
     year: 1926,
-    image: "https://www.thevillagecelebration.com/wp-content/uploads/2018/12/headshot-2-1000x600.jpg"
+    image: "https://www.thevillagecelebration.com/wp-content/uploads/2018/12/headshot-2-1000x600.jpg",
+    id: 5
   },
 
   {
@@ -49,42 +68,59 @@ const quotes = [
     citation: "Malcom X",
     source: "Black Revolution",
     year: 1964,
-    image: "http://www.chrflagship.uwc.ac.za/wp-content/uploads/2018/02/MalcomX3.jpg"
+    image: "http://www.chrflagship.uwc.ac.za/wp-content/uploads/2018/02/MalcomX3.jpg",
+    id: 6
+  }, {
+    quote:
+      "Never forget that intelligence rules the world and ignorance carries the burden. Therefore, remove yourself as far as possible from ignorance and seek as far as possible to be intelligent.",
+    source: "Marcus Garvey Life and Lessons: A Centennial Companion to the Marcus Garvey and Universal Negro Improvement Association Papers",
+    year: 1987,
+    image: "https://www.gannett-cdn.com/-mm-/22542986f934bdefd23b322c85cddc0a56e11090/c=0-351-2375-1693/local/-/media/2018/02/14/USATODAY/USATODAY/636542354546909289-Marcus-Garvey-parade-uniform.jpg?width=3200&height=1680&fit=crop",
+    id: 7
   }
 ];
 
+// arrow functions used throughout program
+// arrow function syntax: (parameters) => {code} vs. function (parameters) {code}
+
 const getRandomQuote = () => {
-  const randomNum = Math.floor(Math.random() * quotes.length);
+  // randomNum variable generates number between 0 & the number
+  // of quote objets in quotes array rolling down the overall number
+  // eliminating any decimal numbers insuring random number is always an integer vs float
+  const randomNum = Math.floor(Math.random() * quotes.length); \
+  //randomQuote variable accesses a random index in quotes array
   const randomQuote = quotes[randomNum];
   return randomQuote;
 }
+// variable responisible for ability to select and in turn manipulate body of entire DOM
+const background = document.querySelector('body');
 
-
-const imageEvent = document.querySelector("#loadQuote");
-const background = document.querySelector('#background');
-
-
+// printQuote function outputs entire quote object to DOM
 const printQuote = () => {
+  //random variable stores the quote stored at the random index in quotes array
   const random = getRandomQuote();
   let printMe = '';
+  // access each specific property found on quotes object and concatenate the...
+  // properties with in the printMe empty string variable which displays to the DOM
+  // with the structure and layout given to it via this program's HTML
   printMe += '<p class="quote">' +
     random.quote + '</p>' + '<p class="source">' +
     random.source + '<span class="citation">' +
     random.citation + '</span>';
+  // control flow used here to determine if a quote has a year associate with it...
+  // if a year is associated with the quote than it is displayed on DOM w/ rest of properties
   if (random.year) {
     printMe += '<span class="year">' + random.year + '</span>';
   } + '</p>';
+  // the structure and location of the quote object's properties are displayed to the DOM...
+  // within the quote-box div
   document.getElementById('quote-box').innerHTML = printMe;
+  // when the quote changes the background randomly changes along with the quote as well
   background.style.backgroundImage = "url(" + random.image + ")";
 }
 
 
-// string interpelation
-//  '<p class="quote">' +
-//   random.quote + '</p>' + '<p class="source">' +
-//   random.source + '<span class="citation">' +
-//   random.citation + '</p>'
-
-
-// string literals
-// `sentence ${variable}`
+// NOTE:
+// Future task(s):
+// • (accessiblity feature with a toggle option) audio playback for each quote
+// • create logic that keeps the program from repeating any quotes
